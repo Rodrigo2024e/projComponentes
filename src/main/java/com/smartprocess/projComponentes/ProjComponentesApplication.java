bbf4f4bbec80fd1f8832530cf2308e0e66e5ac6e
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-11-20 09:54:11 -0300

    Project created

---
+++ src/main/java/com/smartprocess/projComponentes/ProjComponentesApplication.java
@@ -0,0 +1,13 @@
+package com.smartprocess.projComponentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjComponentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjComponentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartprocess/projComponentes/ProjComponentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprocess.projComponentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjComponentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
